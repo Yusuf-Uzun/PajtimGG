74ae043c3beaddc7f87b6cd0a477785ebec969aa
Yusuf Uzun <yusuf3803uzun@gmail.com>
2023-03-16 22:05:05 +0100

    getting value of text box

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from './header/header.component.spec'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
