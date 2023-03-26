bfa49c0b9fd78d9d30b4810826bd1a30f98f7eeb
Yusuf Uzun <yusuf3803uzun@gmail.com>
2023-03-26 18:03:37 +0200

    asd

--- frontend/src/app/summonerpage/summonerpage.component.ts
+++ frontend/src/app/summonerpage/summonerpage.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-summonerpage',
   templateUrl: './summonerpage.component.html',
   styleUrls: ['./summonerpage.component.scss']
 })
-export class SummonerpageComponent {
+export class SummonerpageComponent implements OnInit{
+  readonly ROOT_URL = 'http://localhost:6969/summoners/EUW1/Yusi';
+  //readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
+  dataInfo: any;
+  name: any; 
+  summonerLevel: any;
+  constructor() {}
 
+  ngOnInit(){
+    axios.get(this.ROOT_URL)
+    .then(({data}) => this.dataInfo = data)
+    .then(() => {
+      this.name = this.dataInfo['0'];
+      this.summonerLevel = this.dataInfo['1'] ;
+    })
+    .catch((err) => console.log(err))
+  }
 }
