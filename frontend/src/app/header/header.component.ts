3a51d32484f6454762afe1a43140f23872a5d69a
Yusuf Uzun <yusuf3803uzun@gmail.com>
2023-03-26 17:33:25 +0200

    small fixes

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -4,18 +4,15 @@ import { Location } from '@angular/common';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss','./header.component.normalize.scss']
 })
 export class HeaderComponent {
   constructor(private _location: Location) {}
   getSummoner(val: string){
-    console.warn(val);
-
     const url = `EUW1/${val}`;
 
-    console.warn(url);
-
     this._location.go(url)
+    
     return val;
   }
 }
