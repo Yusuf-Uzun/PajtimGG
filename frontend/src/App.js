24e8a89b2ec9ae4165071e46f00d785bcf53ca1a
Yusuf <yusuf3803uzun@gmail.com>
2023-04-06 23:45:25 +0200

    changing a bit of stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import axios from 'axios';
 import './App.css';
 import './App.normalize.css';
 
 function App() {
+
+  //const summonerName = summonerNameRef.current.value;
+
   return (
     <div>
       <div className="img">
-        <img src="./assets/PajtimGGBanner3.png" width="708mm" height="252mm" alt="Pajtim GG Banner"/>
+        <img src={require("./assets/PajtimGGBanner3.png")} width="708mm" height="252mm" alt="Pajtim GG Banner"/>
       </div>
 
       <div className="bar">
-        <input type="text" placeholder="Summoner Name..." id="summonerName" />
+        <input type="text" placeholder="Summoner Name..." id="summonerName"/>
       </div>
 
       <div className="Search">
-        <a href="summoners/EUW1/Yusi" className="active">
-          <button onClick={getSummoner}>Search</button>
-        </a>
+        <button onClick={getSummoner}>Search</button>
         <button id="na">NA</button>
         <button id="euw">EUW</button>
       </div>
 
       <div className="img2">
         <a href="localhost:4200">
-          <img src="./assets/PajtimGG2.png" width="100px" height="25px" alt="Pajtim GG" />
+          <img src={require("./assets/PajtimGG2.png")} width="100px" height="25px" alt="Pajtim GG" />
         </a>
       </div>
 
@@ -34,6 +36,7 @@ function App() {
 }
 
 function getSummoner() {
+  
   // Implementation for getSummoner function
 }
 
