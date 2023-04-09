ee911be1b4dd459f4df845be41cdcee7d0049c06
Yusuf <yusuf3803uzun@gmail.com>
2023-04-09 11:48:31 +0200

    saving Fpage before change

--- frontend/src/fpage/FPage.tsx
+++ frontend/src/fpage/FPage.tsx
@@ -3,27 +3,25 @@ import GetSummonerName from './getSummonerName';
 import { useState } from 'react';
 
 function FPage(){
-  const [sumName, setSumName] = useState('');
-
-  return(
-      <div>
-      <div className="img">
-        <img src={require("../assets/PajtimGGBanner3.png")} width="708mm" height="252mm" alt="Pajtim GG Banner"/>
-      </div>
-  
-      <div className="bar">
-        <GetSummonerName />
-      </div>
-
-       <div className="img2">
-        <a href="http://localhost:3000">
-          <img src={require("../assets/PajtimGG2.png")} width="100px" height="25px" alt="Pajtim GG" />
-        </a>
-      </div>
-
-       <div className="impressum">
-      </div>
+  return (
+    <div className='test'>
+    <div className="img">
+      <img src={require("../../public/PajtimGGBanner3.png")} width="708mm" height="252mm" alt="Pajtim GG Banner"/>
     </div>
+
+    <div className="bar">
+      <GetSummonerName />
+    </div>
+
+     <div className="img2">
+      <a href="http://localhost:3000">
+        <img src={require("../../public/PajtimGG2.png")} width="100px" height="25px" alt="Pajtim GG" />
+      </a>
+    </div>
+
+     <div className="impressum">
+    </div>
+  </div>
   );
 
 }
