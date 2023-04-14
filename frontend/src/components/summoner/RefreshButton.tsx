f7c0b7f425d82ab42bb0e9349926ff96729f008b
Aaron <aaron.wetzel@web.de>
2023-04-14 23:48:01 +0200

    luv u

--- frontend/src/components/summoner/RefreshButton.tsx
+++ frontend/src/components/summoner/RefreshButton.tsx
@@ -4,7 +4,7 @@ function RefreshButton(){
 
     return (
         <div>
-            <Button style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}} variant="contained" onClick={() => window.location.reload()}>Refresh</Button>
+            <Button style={{maxWidth: '90px', maxHeight: '30px', minWidth: '30px', minHeight: '30px', fontSize: '.8em', backgroundColor: 'rgba(128, 128, 128, 0.3)', marginTop: '50px', marginBottom: '25px', color: 'rgb(20, 40, 80)', borderRadius: '20px'}} variant="contained" onClick={() => window.location.reload()}>Refresh</Button>
         </div>
     )
 }
