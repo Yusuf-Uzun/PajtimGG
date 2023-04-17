e11059bfa66e79b59378ba66810ef406669e357f
Yusuf <yusuf3803uzun@gmail.com>
2023-04-17 10:31:54 +0200

    new module

--- backend/src/constants.py
+++ backend/src/constants.py
@@ -1,5 +1,5 @@
 import os
 
 #LOL_API_KEY = os.environ.get("LOL_API_KEY")
-LOL_API_KEY = 'RGAPI-e1a45219-0708-4ccb-af8c-b03c80e1e969'
+LOL_API_KEY = 'RGAPI-7976a945-41b1-450f-a529-d317b01f21e7'
 url_of_champ_data = 'http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/champion.json'
